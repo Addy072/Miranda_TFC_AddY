db52d96ec985023313b0e3755109a669b6e0b96c
Addy <crodrigueztrillo2006@gmail.com>
2026-01-03 22:50:08 +0100

    3/01/2026 22:49
    Player Controller con necesidad de revision listo

--- Assets/scripts/Player/PlayerController.cs
+++ Assets/scripts/Player/PlayerController.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    private Animator animator;
+
+    
+    public InputActionReference moveActionRef;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Leer el valor desde la referencia
+        movement = moveActionRef.action.ReadValue<Vector2>();
+        movement = movement.normalized;
+
+        animator.SetFloat("moveX", movement.x);
+        animator.SetFloat("moveY", movement.y);
+        animator.SetBool("isMoving", movement.sqrMagnitude > 0.01f);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
+
